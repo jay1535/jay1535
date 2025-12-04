7e36283a365224a15d9f025806cb687ca9445c6f
Jayant R Habbu <habbujayanth@gmail.com>
2025-12-04 17:11:54 +0530

    Add files via upload

---
+++ assets/index.js
@@ -0,0 +1,101 @@
+import css from './css.svg'
+import express from './express.svg'
+import git from './git.svg'
+import github from './github.svg'
+import html from './html.svg'
+import javascript from './javascript.svg'
+import mongodb from './mongodb.svg'
+import motion from './motion.svg'
+import mui from './mui.svg'
+import nextjs from './nextjs.svg'
+import nodejs from './nodejs.svg'
+import react from './react.svg'
+import redux from './redux.svg'
+import sass from './sass.svg'
+import tailwindcss from './tailwindcss.svg'
+import typescript from './typescript.svg'
+import linked from './linked.png'
+import twitter from './twitter.png'
+import car from './car.svg'
+import estate from './estate.svg'
+import pricewise from './pricewise.svg'
+import snapgram from './snapgram.svg'
+import summiz from './summiz.svg'
+import threads from './threads.svg'
+import postman from './Postman.svg'
+import django from "./Django.svg"
+import figma from "./Figma.svg"
+import flask from "./Flask.svg"
+import intellij from "./IntelliJ IDEA.svg"
+import jupyter from "./Jupyter.svg"
+import pycharm from "./PyCharm.svg"
+import vscode from "./VS Code.svg"
+import socket from "./Socket.io.svg"
+import sql from './MySQL.svg'
+import firebase from './Firebase.svg'
+import postgre from './PostgresSQL.svg'
+import arrow from './arrow.svg'
+import contact from './contact.svg'
+import soundon from './soundon.png'
+import soundoff from './soundoff.png'
+import flutter from './flutter.svg'
+import c from './c.svg'
+import python from './python.svg'
+import java from './java.svg'
+import dart from './dart.svg'
+import gcp from './gcp.svg'
+import insta from './insta.png'
+import Next from './Next.js.svg'
+import Jest from './Jest.svg'
+export {
+    css,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    firebase,
+    mongodb,
+    vscode,
+    motion,
+    postman,
+    django,
+    figma,
+    flask,
+    intellij,
+    jupyter,
+    pycharm,
+    socket,
+    sql,
+    postgre,
+    mui,
+    nextjs,
+    nodejs,
+    react,
+    redux,
+    sass,
+    tailwindcss,
+    typescript,
+    Jest,
+    linked,
+    twitter,
+    car,
+    estate,
+    pricewise,
+    snapgram,
+    summiz,
+    threads,
+    arrow,
+    contact,
+    soundon,
+    soundoff,
+    flutter,
+    c,
+    python,
+    java,
+    dart,
+    gcp,
+    insta,
+    Next
+
+}
